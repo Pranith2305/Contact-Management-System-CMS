4b4c5724fea355fa423e278751a8e1ccca3571f9
pranithaiwiz <pranithkumar.8802@gmail.com>
2024-11-14 20:19:12 +0530

    model adde d

---
+++ contact-manager/model.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from .database import Base
+
+class Contact(Base):
+    __tablename__ = "contacts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    phone_number = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+
